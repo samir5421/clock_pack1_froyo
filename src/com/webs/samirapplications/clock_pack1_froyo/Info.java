0853279ed6a5b0efc7714d19dc1141b53a599218
Samir <samir314159@gmail.com>
2010-06-24 16:15:23 -0500

    added froyo apps on sd support to clock pack 1

---
+++ src/com/webs/samirapplications/clock_pack1_froyo/Info.java
@@ -0,0 +1,13 @@
+package com.webs.samirapplications.clock_pack1_froyo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState); setContentView(R.layout.info);
+    }
+}
---
+++ src/com/webs/samirapplications/clock_pack1_froyo/Widget4.java
@@ -0,0 +1,29 @@
+
+package com.webs.samirapplications.clock_pack1_froyo;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class Widget4 extends AppWidgetProvider
+{    
+    public void onReceive(Context context, Intent intent)
+    {
+        String action = intent.getAction();
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(action))
+        {
+       	
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget4);
+            
+            Intent AlarmClockIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER).setComponent(new ComponentName("com.android.alarmclock", "com.android.deskclock"));
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, AlarmClockIntent, 0);
+            views.setOnClickPendingIntent(R.id.Widget, pendingIntent);
+                                    
+            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), views);
+        }
+    }
+}
